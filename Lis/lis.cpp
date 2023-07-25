15d583fbdb0fa2f636eea7c51e433771b549168d
moo00310 <moo00310@naver.com>
2023-07-25 13:21:27 +0900

    LIS

---
+++ Lis/lis.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+using namespace std;
+
+//동적계획법
+//LIS (Longest Increasing Sequence)
+
+//Seq : 1 9 2 5 7
+//부분 수열 : 일부(0개 이상) 숫자를 지우고 남은 수열
+//ex) 1 2 5
+//ex) 1 9 5 7
+
+//순 증가 부분 수열
+//ex) 1 2 5
+ 
+//LIS : 제일 긴[순 증가 부분 수열]의 길이
+// 1 2 5 7 길이->  4
+
+vector<int> seq;
+int cache[100];
+
+int LIS(int pos)
+{
+	//기저 사항
+	/*if (pos == seq.size() - 1)
+		return 1;*/
+
+	//캐쉬 확인
+	int& ret = cache[pos];
+	if (ret != -1)
+		return ret;
+
+	//구하기
+	//Seq : 1 9 2 5 7
+	
+	//1 9
+	//1 2 5 7
+	//1 5 7
+	//1 7
+	ret = 1;
+
+	for (int next = pos + 1; next < seq.size(); next++)
+	{
+		if (seq[pos] < seq[next])
+			ret = max(ret, 1 + LIS(next));
+	}
+
+	return ret;
+}
+
+int main()
+{
+	::memset(cache, -1, sizeof(cache));
+
+	seq = vector<int>{ 1, 9, 2, 5, 7 };
+
+	int ret = 0;
+	for (int pos = 0; pos < seq.size(); pos++)
+		ret = max(ret, LIS(pos));
+
+
+}
